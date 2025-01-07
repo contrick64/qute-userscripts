212f770113c2ae682eab81731c6a4bb2c3c46f4a
Connor Murphy <connor@murphysites.com>
2025-01-07 16:28:21 -0500

    fix quotes and assume commands are on PATH

--- yt.cmd
+++ yt.cmd
@@ -3,11 +3,16 @@ IF "%~1"=="" GOTO end
 set "av_arg=-f best/bestvideo+bestaudio"
 IF "%~1"=="-a" (
   set av_arg=--extract-audio
+  shift
 )
 set args=%*
 set args=%args:-a =%
-set mpv_args=--force-window --autofit=10%% --ontop --geometry=100%%:100%%
-for /f "tokens=*" %%g in ('C:/Users/cmurph52/Downloads/bootstrapper/yt-dlp.exe --get-url %av_arg% ytsearch:%args%') do (SET url=%%g)
-C:/Users/cmurph52/Downloads/bootstrapper/mpv.exe %mpv_args% "%url%"
+if [%2]==[] set args=%~1
+set args="%args%"
+set mpv_args=--force-window --autofit=10%% --ontop --geometry=100%%:100%% --no-border
+for /f "tokens=*" %%g in ('yt-dlp --get-url %av_arg% ytsearch:%args%') do (
+    SET url=%%g
+)
+mpv %mpv_args% "%url%"
 :end
 
